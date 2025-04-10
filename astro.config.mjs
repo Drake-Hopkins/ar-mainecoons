0da507bc5182a7ec6347f021fd78f0ce3892bbfe
DrakeAFK <drakehopkins@outlook.com>
2025-04-09 20:28:00 -0400

    github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://drake-hopkins.github.io',
+  base: 'ar-mainecoons',
   integrations: [tailwind()]
 });
 
