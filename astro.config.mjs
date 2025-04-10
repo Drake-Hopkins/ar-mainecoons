cebda1b078d77b13d230502c757e00231bf01aa2
DrakeAFK <drakehopkins@outlook.com>
2025-04-09 20:41:43 -0400

    img paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://drake-hopkins.github.io',
-  base: 'ar-mainecoons',
+  site: 'https://drake-hopkins.github.io/ar-mainecoons/',
+  base: '/ar-mainecoons/',
   integrations: [tailwind()]
 });
 
